574a0e7a7edd28d855265b703f9c488ccef275e2
Ali Waris <129656913+aliwariss@users.noreply.github.com>
2023-04-12 15:29:07 +0500

    Add files via upload
    objects_nested

---
+++ objects_nested.js
@@ -0,0 +1,21 @@
+let obj = {
+    name: {
+        class: {
+            key: 1
+        },
+        number: "9"
+    },
+    age: 23
+}
+
+/*
+obj.age
+23
+obj.name
+{class: {…}, number: '9'}
+obj.name.number
+'9'
+obj.name.class
+{key: 1}
+obj.name.class.key
+*/
